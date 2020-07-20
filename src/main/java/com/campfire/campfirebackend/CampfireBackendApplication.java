27b91985570b42aae20247a810df29cae86dfc19
Kelso <graemebrooks@gmail.com>
2020-07-20 07:41:16 -0500

    initial commit

---
+++ src/main/java/com/campfire/campfirebackend/CampfireBackendApplication.java
@@ -0,0 +1,13 @@
+package com.campfire.campfirebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampfireBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampfireBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campfire/campfirebackend/CampfireBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campfire.campfirebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampfireBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
