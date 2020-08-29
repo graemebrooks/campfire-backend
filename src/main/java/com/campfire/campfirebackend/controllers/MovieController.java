26645a2b5b8916c9a3cdc834d6b05bce0a7e9d1b
Kelso <graemebrooks@gmail.com>
2020-08-29 05:40:32 -0500

    adds add and update movie functionality

--- src/main/java/com/campfire/campfirebackend/controllers/MovieController.java
+++ src/main/java/com/campfire/campfirebackend/controllers/MovieController.java
@@ -1,8 +1,7 @@
 package com.campfire.campfirebackend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,6 +14,21 @@ public class MovieController {
     @RequestMapping("/api/movies")
     public List<Movie> getAllMovies() {
         return movieService.getAllMovies();
+    }
+
+    @RequestMapping("/api/movies/{id}")
+    public Movie getMovie(@PathVariable String id) {
+        return movieService.getMovie(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/api/movies")
+    public void addMovie(@RequestBody Movie movie) {
+        movieService.addMovie(movie);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/api/movies/{id}")
+    public void updateMovie(@RequestBody Movie movie, @PathVariable String id) {
+        movieService.updateMovie(id, movie);
     }
 
 }
--- src/main/java/com/campfire/campfirebackend/controllers/MovieService.java
+++ src/main/java/com/campfire/campfirebackend/controllers/MovieService.java
@@ -2,19 +2,38 @@ package com.campfire.campfirebackend.controllers;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class MovieService {
 
-    private List<Movie> movies = Arrays.asList(
+    private List<Movie> movies = new ArrayList<>(Arrays.asList(
             new Movie("movie-1", "Jurassic Park", 1993),
             new Movie("movie-2", "Her", 2013),
             new Movie("movie-3", "Get Out", 2017)
-    );
+    ));
 
     public List<Movie> getAllMovies() {
         return movies;
+    }
+
+    public Movie getMovie(String id) {
+        return movies.stream().filter(m -> m.getId().equals(id)).findFirst().get();
+    }
+
+    public void addMovie(Movie movie) {
+        movies.add(movie);
+    }
+
+    public void updateMovie(String id, Movie movie) {
+        for (int i = 0; i < movies.size(); i++) {
+            Movie m = movies.get(i);
+            if (m.getId().equals(id)) {
+                movies.set(i, movie);
+                return;
+            }
+        }
     }
 }
